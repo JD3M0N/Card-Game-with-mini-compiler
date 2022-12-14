328c766c9e6d20fbe68f35494bc4575c20d98ae4
JD3M0N <josuedeki21@gmail.com>
2022-12-13 23:55:44 -0500

    textComit

--- ConsoleGameApp/ConsoleGameApp/Program.cs
+++ ConsoleGameApp/ConsoleGameApp/Program.cs
@@ -7,7 +7,7 @@ namespace War_Game
 {
     public class Main_Game
     {
-        // esto es una prueba asd
+        // esto es una prueba asdsss
         static void Main(string[] args)
         {
             string prompt = @"

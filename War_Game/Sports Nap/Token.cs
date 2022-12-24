dbf236247de266031c2843de96d1171adc13e2a3
GAlonso46 <118123369+GAlonso46@users.noreply.github.com>
2022-12-24 06:24:13 +0100

    Parse and Tokens

---
+++ War_Game/Sports Nap/Token.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace War_Game
+{
+    public class Token
+    {
+        public string Type;
+        public string Description;
+
+        public Token(string type, string description)
+        {
+            Type = type;
+            Description = description;
+        }
+    }
+}
